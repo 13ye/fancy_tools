c8d83ecfb0e9dd758225b1ed1bd8b3ccd76ade4d
lfj <2559877637@qq.com>
2022-05-05 15:17:59 +0800

    version update

--- with_docker/trilium/run_docker.sh
+++ with_docker/trilium/run_docker.sh
@@ -3,10 +3,10 @@
 # Data Folder For Docs...
 TRILIUMDATAPATH=/home/trilium-data
 mkdir -p $TRILIUMDATAPATH
-chmod 777 $TRILIUMDATAPATH
+sudo chmod 744 $TRILIUMDATAPATH
 
 docker run -itd \
     --name trilium_doc \
     -p 27543:8080 \
     -v $TRILIUMDATAPATH:/home/node/trilium-data \
-    zadam/trilium:0.50.3
+    zadam/trilium:0.51.2
