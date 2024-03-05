5783f135162e92f29ff6ab49b31977ae4c5db3fd
13ye <2559877637@qq.com>
2024-03-05 20:28:33 +0800

    upd

--- with_docker/filebrowser/run_docker.sh
+++ with_docker/filebrowser/run_docker.sh
@@ -4,13 +4,14 @@
 FILEBROWSER_HOME=/home/filebrowser
 mkdir -p $FILEBROWSER_HOME
 chmod 777 $FILEBROWSER_HOME
+touch $FILEBROWSER_HOME/filebrowser.db
 
 docker run -itd \
     --name filebrowser \
     -v $FILEBROWSER_HOME/root:/srv \
-    -v $FILEBROWSER_HOME/filebrowser.db:/database/filebrowser.db \
+    -v $FILEBROWSER_HOME/filebrowser.db:/database.db \
     -u $(id -u):$(id -g) \
     -p 14480:80 \
-    filebrowser/filebrowser:v2.21.1
+    filebrowser/filebrowser:v2.27.0
 
     #-v $FILEBROWSER_HOME/.filebrowser.json:/.filebrowser.json \
